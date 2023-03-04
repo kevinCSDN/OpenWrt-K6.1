c0a4316a0023c46dc08187f7ed8770206821c870
ArronPlus <109497993+ArronPlus@users.noreply.github.com>
2023-03-04 13:09:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,5 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/f
 sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
 # Change the kernel
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefile
+# Change the version
+sed -i 's/R23.3.3//g' package/lean/default-settings/files/zzz-default-settings
